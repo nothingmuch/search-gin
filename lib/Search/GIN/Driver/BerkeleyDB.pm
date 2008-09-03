07001afe476d6762ce6e08a55f1921dd2542139f
Yuval Kogman <nothingmuch@woobling.org>
2008-09-03 22:12:04 +0000

    cleanup BDB open flags

--- lib/Search/GIN/Driver/BerkeleyDB.pm
+++ lib/Search/GIN/Driver/BerkeleyDB.pm
@@ -53,7 +53,7 @@ sub _build_env {
 
     BerkeleyDB::Env->new(
         -Home  => $self->home,
-        -Flags => DB_CREATE|DB_INIT_TXN|DB_INIT_MPOOL|DB_INIT_LOCK,
+        -Flags => DB_CREATE | DB_INIT_MPOOL | DB_INIT_TXN,
     );
 }
 
