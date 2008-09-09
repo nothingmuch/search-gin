c46581683b8206d2600433aa19bcb934a56bc6cd
Yuval Kogman <nothingmuch@woobling.org>
2008-09-09 17:07:52 +0000

    index by role name, not by role meta object

--- lib/Search/GIN/Extract/Class.pm
+++ lib/Search/GIN/Extract/Class.pm
@@ -20,7 +20,7 @@ sub extract_values {
     my $isa = $class->mro::get_linear_isa();
 
     my $meta = Class::MOP::get_metaclass_by_name($class);
-    my @roles = $meta && $meta->can("calculate_all_roles") ? $meta->calculate_all_roles : ();
+    my @roles = $meta && $meta->can("calculate_all_roles") ? ( map { $_->name } $meta->calculate_all_roles ) : ();
 
     return $self->process_keys({
         blessed => $class,
