c5f0ad19219b2d1778dbe3a4023dedddc8d3d24c
Yuval Kogman <nothingmuch@woobling.org>
2008-09-23 15:39:31 +0000

    add version stub

---
+++ lib/Search/GIN.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+package Search::GIN;
+
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+__PACKAGE__
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Search::GIN - 
+
+=head1 SYNOPSIS
+
+	use Search::GIN;
+
+=head1 DESCRIPTION
+
+=cut
+
+
