27527047633493426f3ee9549248eb079c8e2707
Sawyer X <xsawyerx@cpan.org>
2010-03-04 13:23:34 +0200

    fixing copy right, bumping version to 0.04

--- lib/Search/GIN.pm
+++ lib/Search/GIN.pm
@@ -5,7 +5,7 @@ package Search::GIN;
 use strict;
 use warnings;
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 __PACKAGE__
 
@@ -29,7 +29,7 @@ More documentation will be released shortly.
 
 =head1 COPYRIGHT
 
-    Copyright (c) 2008, 2009 Yuval Kogman, Infinity Interactive. All
+    Copyright (c) 2008 - 2010 Yuval Kogman, Infinity Interactive. All
     rights reserved This program is free software; you can redistribute
     it and/or modify it under the same terms as Perl itself.
 
