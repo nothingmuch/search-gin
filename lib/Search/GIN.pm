44b5c1c46617d1ea880d6ed69a1cf6f985e1c32e
Sawyer X <xsawyerx@cpan.org>
2010-09-08 16:48:29 +0300

    adding more description

--- lib/Search/GIN.pm
+++ lib/Search/GIN.pm
@@ -17,15 +17,27 @@ __END__
 
 Search::GIN - Generalized Inverted Indexing
 
-=head1 SYNOPSIS
-
-	# Coming Soon
-
 =head1 DESCRIPTION
 
-This is used by L<KiokuDB> for custom indexing.
+Inverted Indexing is an indexing method that maps from content to location in
+storage.
 
-More documentation will be released shortly.
+Generalized Inverted Indexing (GIN, for short) is an inverted indexing method
+in which the index is unaware of what data exactly is it indexing.
+
+L<Search::GIN> is primarily used by L<KiokuDB> for custom indexing.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<pgsql-hackers msg #00960|http://archives.postgresql.org/pgsql-hackers/
+2006-04/msg00960.php>
+
+=item * L<Inverted_index on Wikipedia|http://en.wikipedia.org/wiki/
+Inverted_index>
+
+=back
 
 =head1 COPYRIGHT
 
